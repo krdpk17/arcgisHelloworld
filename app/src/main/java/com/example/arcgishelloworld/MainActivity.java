8c551c70f7e274f6f30cab5c1a5f2cb2aea15944
krdpk1717 <62544105+krdpk1717@users.noreply.github.com>
2020-05-28 15:40:05 +0530

    Example of map 3D view

--- app/src/main/java/com/example/arcgishelloworld/MainActivity.java
+++ app/src/main/java/com/example/arcgishelloworld/MainActivity.java
@@ -5,21 +5,31 @@ import android.os.Bundle;
 
 import com.esri.arcgisruntime.ArcGISRuntimeEnvironment;
 import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.ArcGISScene;
 import com.esri.arcgisruntime.mapping.Basemap;
-import com.esri.arcgisruntime.mapping.view.MapView;
+//import com.esri.arcgisruntime.mapping.view.MapView;
+import com.esri.arcgisruntime.mapping.view.Camera;
+import com.esri.arcgisruntime.mapping.view.SceneView;
 
 public class MainActivity extends AppCompatActivity {
-    private MapView mMapView;
+    private SceneView mSceneView;
 
     private void setupMap() {
-        if (mMapView != null) {
+        if (mSceneView != null) {
             ArcGISRuntimeEnvironment.setLicense(getResources().getString(R.string.arcgis_license_key));
-            Basemap.Type basemapType = Basemap.Type.STREETS_VECTOR;
-            double latitude = 12.9716;
-            double longitude = 77.5946;
-            int levelOfDetail = 13;
-            ArcGISMap map = new ArcGISMap(basemapType, latitude, longitude, levelOfDetail);
-            mMapView.setMap(map);
+
+            double latitude = 33.8610;
+            double longitude = -118.8080;
+            double altitude = 25000.0;
+            double heading = 0.1;
+            double pitch = 45.0;
+            double roll = 0.0;
+
+            ArcGISScene scene = new ArcGISScene();
+            scene.setBasemap(Basemap.createStreets());
+            mSceneView.setScene(scene);
+            Camera camera = new Camera(latitude, longitude, altitude, heading, pitch, roll);
+            mSceneView.setViewpointCamera(camera);
         }
     }
 
@@ -28,13 +38,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mMapView = findViewById(R.id.mapView);
+        mSceneView = findViewById(R.id.sceneView);
         setupMap();
     }
     @Override
     protected void onPause() {
-        if (mMapView != null) {
-            mMapView.pause();
+        if (mSceneView != null) {
+            mSceneView.pause();
         }
         super.onPause();
     }
@@ -42,15 +52,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        if (mMapView != null) {
-            mMapView.resume();
+        if (mSceneView != null) {
+            mSceneView.resume();
         }
     }
 
     @Override
     protected void onDestroy() {
-        if (mMapView != null) {
-            mMapView.dispose();
+        if (mSceneView != null) {
+            mSceneView.dispose();
         }
         super.onDestroy();
     }
