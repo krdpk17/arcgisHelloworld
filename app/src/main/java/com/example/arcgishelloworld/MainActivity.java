65400ac9bdfdf7d2babd729cc06402758c566fef
krdpk1717 <62544105+krdpk1717@users.noreply.github.com>
2020-05-28 15:19:30 +0530

    Initial commit

---
+++ app/src/main/java/com/example/arcgishelloworld/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.arcgishelloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.esri.arcgisruntime.ArcGISRuntimeEnvironment;
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.MapView;
+
+public class MainActivity extends AppCompatActivity {
+    private MapView mMapView;
+
+    private void setupMap() {
+        if (mMapView != null) {
+            ArcGISRuntimeEnvironment.setLicense(getResources().getString(R.string.arcgis_license_key));
+            Basemap.Type basemapType = Basemap.Type.STREETS_VECTOR;
+            double latitude = 12.9716;
+            double longitude = 77.5946;
+            int levelOfDetail = 13;
+            ArcGISMap map = new ArcGISMap(basemapType, latitude, longitude, levelOfDetail);
+            mMapView.setMap(map);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mMapView = findViewById(R.id.mapView);
+        setupMap();
+    }
+    @Override
+    protected void onPause() {
+        if (mMapView != null) {
+            mMapView.pause();
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mMapView != null) {
+            mMapView.resume();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mMapView != null) {
+            mMapView.dispose();
+        }
+        super.onDestroy();
+    }
+}
